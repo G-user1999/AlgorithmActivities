aadd3d9ebe1a25a6bbc36d66c4b1c1c75702e0a2
G-user1999 <Gabrielpadilh@gmail.com>
2023-11-16 11:03:06 -0300

    dasd

--- Misc/SystemGabriel.js
+++ Misc/SystemGabriel.js
@@ -1,2 +1,16 @@
 const reader = require("readline-sync");
-
+//1)
+A = reader.questionInt("Give number A: ")
+B = reader.questionInt("Give number B: ")
+function Sum(A,B) {
+   return A + B
+}
+function Multiply(A,B) {
+    return A * B
+}
+console.log("A and B summed is",Sum(A,B),"and multiplied it's",Multiply(A,B));
+//2)
+ztring = reader.question("Write a phrase: ")
+function inverterString(ztring) {
+    
+}
