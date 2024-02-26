3dc4581a8815b66e060da5c8b435511645ece0ce
G-user1999 <Gabrielpadilh@gmail.com>
2024-02-26 10:36:42 -0300

    ll

--- Misc/Interfaces/activities.ts
+++ Misc/Interfaces/activities.ts
@@ -2,10 +2,15 @@ for (let i = 1; i < 6; i++) {
 console.log(i);
 }
 
-let colors: string[] =["red","blue","green"]
+let colors:string[] =["red","blue","green"]
 
 let tone ={
     "red":"dark",
     "blue":"light",
     "green":"light"
 }
+
+ colors.forEach(element => {
+    console.log(element);
+    }
+);
