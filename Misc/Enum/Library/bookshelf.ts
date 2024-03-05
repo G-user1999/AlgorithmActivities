2590960065b265503280569d55909c3e13b916d0
G-user1999 <Gabrielpadilh@gmail.com>
2024-03-05 11:49:27 -0300

    çç

---
+++ Misc/Enum/Library/bookshelf.ts
@@ -0,0 +1,42 @@
+import { library } from "./library";
+
+function unit1(name:string,author:string) {
+    name = "Moby-dick"
+    author = "Herman Melville"
+}
+
+function unit2(name:string,author:string) {
+    name = "Don Quixote"
+    author = "Miguel de Cervantes"
+}
+
+function unit3(name:string,author:string) {
+    name = "Wuthering Heights"
+    author = "Emily Bronte"
+}
+
+function unit4(name:string,author:string) {
+    name = "Frankenstein"
+    author = "Mary Shelley"
+}
+
+function unit5(name:string,author:string) {
+    name = "Lord of the Flies"
+    author = "William Golding"
+}
+
+function unit6(name:string,author:string) {
+    name = "The Last Unicorn"
+    author = "Peter S. Beagle"
+}
+
+function unit7(name:string,author:string) {
+    name = "Alices Adventures in Wonderland"
+    author = "Lewis Carroll"
+}
+
+function unit8(name:string,author:string) {
+    name = "The Lion, the Witch and the Wardrobe"
+    author = "C.S. Lewis"
+}
+
