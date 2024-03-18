c37243f006db03ae7e3e239e3dddc96bf7fd16a6
G-user1999 <Gabrielpadilh@gmail.com>
2024-03-18 09:20:15 -0300

    ll

--- Misc/Enum/Library/Index.ts
+++ Misc/Enum/Library/Index.ts
@@ -1,4 +1,5 @@
 import { Status } from "./ENUMS/Status";
 import { Genre } from "./ENUMS/Genre";
+import { Library } from "./Classes/Library";
 
 //neste arquivo vão ser feitas as instancias das classes, e vão ser chamados todos os métodos
